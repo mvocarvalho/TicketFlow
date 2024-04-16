f3cae5ba2875a4cd7d67002c06ec9b5b2e340ea3
mvocarvalho <marcos.carvalho@deal.com.br>
2024-04-15 23:33:16 -0300

    Initial commit

---
+++ src/main/java/com/porto/unit/squad18fabtec/Squad18FabtecApplication.java
@@ -0,0 +1,13 @@
+package com.porto.unit.squad18fabtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Squad18FabtecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Squad18FabtecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porto/unit/squad18fabtec/Squad18FabtecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porto.unit.squad18fabtec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Squad18FabtecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
