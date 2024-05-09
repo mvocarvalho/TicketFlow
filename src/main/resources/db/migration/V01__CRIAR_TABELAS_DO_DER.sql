e7657094215c4d6586d5a9af97398d09a5a0988c
mvocarvalho <marcos.carvalho@deal.com.br>
2024-05-09 20:36:15 -0300

    Ajustes de dependencias e criação de script simples

---
+++ src/main/resources/db/migration/V01__CRIAR_TABELAS_DO_DER.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Usuario (
+	id INT PRIMARY KEY,
+	login varchar(60),
+	senha varchar(60)
+);
---
+++ src/main/resources/db/migration/V02__INSERIR_DADOS_FICTICIOS.sql
@@ -0,0 +1,5 @@
+-- Inserção de dados na tabela Usuario
+INSERT INTO Usuario (id, login, senha) VALUES
+(1, 'fulano@email.com', 'senha123'),
+(2, 'ciclano@email.com', 'senha456'),
+(3, 'beltrano@email.com', 'senha789');
